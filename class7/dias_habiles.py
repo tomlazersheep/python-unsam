15e88335fe97b1c5c5b8d7b9a8c08dfaf2296ec5
tomlazersheep <loveratomasg@gmail.com>
2020-09-23 20:15:39 -0300

    dias_habiles done

--- class7/dias_habiles.py
+++ class7/dias_habiles.py
@@ -17,13 +17,26 @@ import datetime
 feriados = ['12/10/2020', '23/11/2020', '7/12/2020', '8/12/2020', '25/12/2020']
 
 def get_working_days (initial, final, non_working_days=[]):
+    # consigo objeto con la fecha inicial
     initial_date = datetime.datetime.strptime(initial, '%d/%m/%Y')
+    
+    # consigo objeto con la fecha final
     final_date = datetime.datetime.strptime(final, '%d/%m/%Y')
     
+    # consigo todos los objetos con las fechas de feriados
     non_working_days_dates = [datetime.datetime.strptime(date, '%d/%m/%Y') for date in non_working_days]
     
-    non_working_total_timedelta = datetime.timedelta(days=0)
+    # armo timedelta para ir sumando los días no laborables
+    non_working_total_timedelta = datetime.timedelta(days=-1)
 
+    # consigo los días totales entre inicio y final
     total_days = final_date - initial_date
+
+    # itero por todos los días entre inicio y final, agregando los días no laborables a mi contador
+    for single_date in (initial_date + datetime.timedelta(days=n) for n in range(total_days.days)):
+        if (single_date.isoweekday() in (5, 6)) or (single_date in non_working_days_dates):
+           non_working_total_timedelta += datetime.timedelta(days=1)
+
+    return (total_days - non_working_total_timedelta).days
 
 print(get_working_days('23/09/2020','10/10/2020',feriados))
